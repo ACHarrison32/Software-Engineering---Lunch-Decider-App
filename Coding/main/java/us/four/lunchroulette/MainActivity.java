c28371fc10b00810680022d4ec8791f670521301
bfce <bfce@pop-os.localdomain>
2023-05-03 12:34:07 -0500

    add comments, make popup faster

--- Coding/main/java/us/four/lunchroulette/MainActivity.java
+++ Coding/main/java/us/four/lunchroulette/MainActivity.java
@@ -62,28 +62,31 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //required by android
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Can't enable wheel image until after content view is set
-        ImageView wheelImage = findViewById(R.id.imageView2);
-//        wheelImage.setVisibility(View.INVISIBLE);
-
-        //Access GPS from the user.
-        //TODO: Fallback method if this doesn't work, allow ZIP input
-        GPSTracker tracker = new GPSTracker(this);
-        gpsTracker = tracker;
-        Map<String, String> params = this.makeParameterMap();
-        params.put("latitude", tracker.getLatitude() + "");
-        params.put("longitude", tracker.getLongitude() + "");
-//        params.put("latitude", "33.930828");
-//        params.put("longitude", "-98.484879");
-        this.callYelp(params);
+        //setup onclick listeners
         findViewById(R.id.button).setOnClickListener(this::filtersButton_Click);
         findViewById(R.id.searchForButton).setOnClickListener(this::searchForButton_Click);
         findViewById(R.id.imageView2).setOnClickListener(this::spin);
-        this.runChangedItemScanner(tracker);
 
+        //Access GPS from the user.
+        GPSTracker tracker = new GPSTracker(this);
+        //set gpsTracker public. Since we have a public instance
+        //of the MainActivity, that means other parts of the app
+        //can access the location without re-calling
+        gpsTracker = tracker;
+        //get a list of parameters
+        Map<String, String> params = this.makeParameterMap();
+        //add location
+        params.put("latitude", tracker.getLatitude() + "");
+        params.put("longitude", tracker.getLongitude() + "");
+        //call yelp with those initial params.
+        this.callYelp(params);
+        //call our secondary thread that scans for currentFilter changes
+        this.runChangedItemScanner(tracker);
+        //create a local file manager and read the users favorites from file.
         FileManager manager = new FileManager();
         try {
             favorites = manager.readFavoritesFromFile(this);
@@ -91,42 +94,68 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
             favorites = new HashSet<>();
         }
+        //initialize the public instance for other classes
+        //There might be a better way to grab a parent activity
+        //in android, but wasn't able to find it in time.
+        //A static instance is perfectly acceptable though, since
+        //there will only ever be one active instance.
         INSTANCE = this;
     }
 
+    /**
+     * Input: none
+     * @return Map<String, String> of yelp parameters
+     * Uses information from the Filters file to make a
+     * list of parameters to send to yelp
+     */
     public Map<String, String> makeParameterMap() {
+        //create local map
         Map<String, String> params = new HashMap<>();
+        //grab current user preferences
         Preferences pref = this.getCurrentPreference();
-        if(pref != null)
+        //null checks with defaults are needed like everywhere
+        if(pref != null) //grab radius from filters if its available
             params.put("radius", String.valueOf(pref.getDistance()));
-        else
+        else //otherwise, default to 20 miles (yelp takes radius in meters)
             params.put("radius", "32186");
+
         if(pref != null) {
             if (pref.getPriceRange() != 0) {
                 //we have to put every price range *up to* the one selected
+                //for example if the user selects $$$
+                //we want it to also show $ and $$
                 String priceRange = "";
+                //create the string depending on the int value
                 for (int i = 1; i <= pref.getPriceRange(); i++) {
                     priceRange = priceRange + i + ", ";
                 }
+                //remove the trailing ', '
                 priceRange = priceRange.substring(0, priceRange.length() - 2);
-                System.out.println(priceRange + " pricerange");
+                //add to params
                 params.put("price", priceRange);
             }
         }
+        //we only show restaurants that are open
         params.put("open_now", "true");
+        //Getting the category is one of the hardest parts
+        //default is 'restaurants' - shows all categories
         String category = "restaurants";
         if(pref != null && pref.getFoodType() != null) {
             switch (pref.getFoodType()) {
-                case AMERICAN:
+                case AMERICAN: //To add 2 categories, use ',' delimiter
+                                //This functions as (newamerican || tradamerican)
+                                //and there is no way to do newamerican && tradamerican)
                     category = "newamerican,tradamerican";
                     break;
                 case ITALIAN:
+                    //All of these categories were pulled from yelps dev portal
                     category = "italian";
                     break;
                 case MEXICAN:
                     category = "mexican,brazilian,newmexican,spanish,tex-mex";
                     break;
                 case ASIAN:
+                    //we current group all asian food together
                     category = "chinese,japanese,thai,vietnamese,noodles,ramen,indian";
                     break;
                 case VEGETARIAN:
@@ -312,15 +341,20 @@ public class MainActivity extends AppCompatActivity {
         boolean focusable = true; // lets taps outside the popup also dismiss it
         PopupWindow popupWindow = new PopupWindow(popupView, width, height, focusable);
 
-        //TODO: COMMENT THIS
+        //We want to get the background color appropriate for the users theme
+        //fallback to white
         int color = 0xFFFFFFFF;
         TypedValue typedValue = new TypedValue();
+        //grab system theme attribute
         if (this.getTheme().resolveAttribute(android.R.attr.windowBackground, typedValue, true))
         {
+            //replace
             color = typedValue.data;
         }
 
+        //set window color to the theme color we grabbed
         popupWindow.setBackgroundDrawable(new ColorDrawable(color));
+        //set elevation controls how big the "shadow" is
         popupWindow.setElevation(20);
 
        // popupWindow.set
@@ -416,51 +450,79 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    /**
+     * Returns a Drawable image from the provided url
+     * @param url
+     * @return
+     */
     public Drawable getImageFromUrl(String url) {
         try {
+            //get inputStream
             InputStream is = (InputStream) new URL(url).getContent();
+            //use android createFromString method
             return Drawable.createFromStream(is, "src name");
         } catch (Exception e) {
+            //internet failed? bad link?
             e.printStackTrace();
             return null;
         }
     }
 
+    /**
+     *
+     * Adds information to all textViews and ImageView
+     *
+     * @param Business b
+     * @param view
+     * @param Drawable img
+     */
     @SuppressLint("SetTextI18n")
     private void fillRestaurant(Business b, View view, Drawable img) {
+        //get instances of TextViews and ImageView
         TextView name = view.findViewById(R.id.nameText);
         TextView rating = view.findViewById(R.id.ratingText);
         TextView phone = view.findViewById(R.id.phoneText);
         TextView distance = view.findViewById(R.id.distanceText);
         ImageView image = view.findViewById(R.id.restaurantImage);
+
+        //we can't use half of a star currently :(
         int stars = (int) Math.floor(b.getRating());
+
+        //Builds a string of stars for the rating
         StringBuilder s = new StringBuilder();
         for(int i = 0; i < stars; i++) {
             s.append("⭐");
         }
+        //append the decimal value after the emojis
         s.append(" (").append(b.getRating()).append(")");
+        //the rating text
         rating.setText(s.toString());
+        //set the name
         name.setText(b.getName());
+        //set the image
         image.setImageDrawable(img);
+        //set the distance, converted from meters to miles
         distance.setText(Math.round(b.getDistance() * 0.0006213712) + " Miles Away");
+        //set the phone number
         phone.setText(b.getDisplayPhone());
     }
 
+    /**
+     * Opens the filters intent
+     * @param view
+     */
     private void filtersButton_Click(View view) {
-
         Intent intent = new Intent("filters.intent.action.Launch");
         startActivity(intent);
-//        if(filter.getPreferencesList() == null && this.prefs != null) {
-//            filter.setPreferencesList(this.prefs);
-//        }
     }
-    private void searchForButton_Click(View view) {
 
+    /**
+     * Opens the search intent
+     * @param view
+     */
+    private void searchForButton_Click(View view) {
         Intent intent = new Intent("search.intent.action.Launch");
         startActivity(intent);
-//        if(filter.getPreferencesList() == null && this.prefs != null) {
-//            filter.setPreferencesList(this.prefs);
-//        }
     }
 
     //seperate rotation int to be stored
@@ -470,16 +532,17 @@ public class MainActivity extends AppCompatActivity {
     private void spin(View view) {
 
 
-        //Commented out code was for showing the result of the spin
-        //WILL BE USED LATER
 
         //IMPORTANT!! WE DON'T CHOOSE A RANDOM RESTURANT FROM THE SPIN
         //INSTEAD WE CHOOSE A RANDOM ROTATION
         //FROM THERE WE USE MATH TO FIND THE RESTURANT
         //FOR EXAMPLE: WHICH RESTURANT IS 790 DEGREES OF ROTATION?
 
-        String[] roast = wheelText;
-        if(roast.length == 0)
+
+        String[] wheelRestaurantNames = wheelText;
+
+        //Fixes a crash when tapping on a blank wheel
+        if(wheelRestaurantNames.length == 0)
             return;
 
         //determines how much the wheel should spin
@@ -495,9 +558,16 @@ public class MainActivity extends AppCompatActivity {
 
         //used for getting the result
         //crazy math :)
-        int segmentLength = (360/roast.length);
-        int result = Math.min((int) Math.ceil(((segmentLength+(360-currentRotation))) / segmentLength), roast.length);
-        String restaurant = roast[result-1];
+        //first get the segment length
+        int segmentLength = (360/wheelRestaurantNames.length);
+        //then we get the relative rotation of the wheel in a range of 360 degrees
+        //and divide that by the segment length, that will be which segment we landed on
+        //however, it could return results like 4.1. 4.1 visually is really on the 5th segment
+        //so we take the ceiling of the result
+        //Because we take the ceiling, its possible to get a result like (length + 0.9) which ceils to length + 1
+        //That would cause an indexOutOfBounds, so we take the Math.min between the result and the total length
+        int result = Math.min((int) Math.ceil(((segmentLength+(360-currentRotation))) / segmentLength), wheelRestaurantNames.length);
+        String restaurant = wheelRestaurantNames[result-1];
 
 
         //The animation interpolator determines how the 'physics' of the spin look
@@ -505,32 +575,57 @@ public class MainActivity extends AppCompatActivity {
         anim.setInterpolator(new AccelerateDecelerateInterpolator());
         //how long the animation lasts in ms
         //also determines speed... longer animation, slower spin.
-        anim.setDuration(1500);
+        int duration = 1500;
+        anim.setDuration(duration);
 
         //needed for the animation to not reset after the spin
         anim.setFillEnabled(true);
         anim.setFillAfter(true);
 
-        //Go!!
+        //start the animation
         refreshImage.startAnimation(anim);
-
-        //TODO: Comment this chunk
+        //grab the system time to make sure the thread waits enough
+        long time = System.currentTimeMillis();
+        //We want to display a popup after the animation plays
+        //however, we need to wait for the animation to finish
+        //and we need to start downloading the restaurant image as soon
+        //as possible so the user doesn't have to wait
+        //So, we make a thread.
         MainActivity activity = this;
         Executor executor = command -> new Thread(command).start();
         executor.execute(() -> {
-            try {
-                Thread.sleep(1500);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
+            //grab the restaurant that the wheel chose
             Business business = null;
             for(Business b : restaurants) {
                 if (b.getName().startsWith(restaurant)) {
                     business = b;
                 }
             }
+            //start downloading the image asap
             assert business != null;
             Drawable img = this.getImageFromUrl(business.getImageUrl());
+            //the amount of time that has passed while downloading the image
+            //in milliseconds
+            long timeTaken = System.currentTimeMillis()-time;
+            //the download was faster than the animation
+            if(timeTaken < duration) {
+                //then we need to wait for the animation
+                //offset is the extra time we wait after the animation ends
+                //so that the user can see their result for a moment.
+                //zero makes the popup display instantly after the wheel stops
+                int offset = 200;
+                int waitTime = (int) ((duration - timeTaken) + offset);
+                try {
+                    Thread.sleep(waitTime);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            //If the download was slower then the animation, we don't wait at all
+            //display it ASAP!
+
+            //Run the deployPopup code on the main thread, you can't do it on
+            //non ui-threads
             activity.runOnUiThread(() -> deployPopup(view, restaurant, img));
         });
 
